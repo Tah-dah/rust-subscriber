b7340e5c2f4eed5c04fc4a208d10be1b35e17797
tah-dah <markcfranz13@gmail.com>
2022-12-15 15:17:19 -0600

    bash docker

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -24,3 +24,13 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
+
+“# Launch postgres using Docker
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000”
+

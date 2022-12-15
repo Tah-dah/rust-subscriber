14622b48ef29a3e772899abdb955bcec434f5499
tah-dah <markcfranz13@gmail.com>
2022-12-15 16:08:42 -0600

    migration subs

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -25,7 +25,7 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-“# Launch postgres using Docker
+# Launch postgres using Docker
 docker run \
   -e POSTGRES_USER=${DB_USER} \
   -e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -33,4 +33,18 @@ docker run \
   -p "${DB_PORT}":5432 \
   -d postgres \
   postgres -N 1000”
+
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
+
+
 
